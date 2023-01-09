7bc1be4638be5786eabfbdc01975ed6da3285f18
Chris Celaya <im-mr-chris@protonmail.ch>
2023-01-08 23:56:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# cd into the repo
+cd gpt3-writer-starter
+
+# install next if you don't have it
+yarn add next react react-dom
+
+# create .env
+
+# run it
+yarn dev
---
+++ pages/api/generate.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.8,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
